89e6fc22104dce654f5c9b1b6ebd02550cecc294
warr <123767168@qq.com>
2022-03-04 14:20:37 +0800

    Update chainutil.go

--- ethutil/chainutil.go
+++ ethutil/chainutil.go
@@ -18,40 +18,40 @@ type TxBaseParams struct {
 	gasPrice *big.Int
 }
 
-func getNextNonce(client *ethclient.Client, account string) uint64 {
+func GetNextNonce(client *ethclient.Client, account string) uint64 {
 	nonce, err := client.NonceAt(context.Background(), common.HexToAddress(account), big.NewInt(rpc.LatestBlockNumber.Int64()))
 	for err != nil {
-		logWithTime(fmt.Sprintf("get %s nonce err: %s,sleep 1s...", account, err.Error()))
+		LogWithTime(fmt.Sprintf("get %s nonce err: %s,sleep 1s...", account, err.Error()))
 		time.Sleep(time.Second)
 
 		nonce, err = client.NonceAt(context.Background(), common.HexToAddress(account), big.NewInt(rpc.LatestBlockNumber.Int64()))
 	}
-	logWithTime(fmt.Sprintf("%s next nonce: %d", account, nonce))
+	LogWithTime(fmt.Sprintf("%s next nonce: %d", account, nonce))
 
 	return nonce
 }
 
-func waitTxReceipt(client *ethclient.Client, txId string, txDesc string, maxQuerySeconds int64) bool {
+func WaitTxReceipt(client *ethclient.Client, txId string, txDesc string, maxQuerySeconds int64) bool {
 	timeStart := time.Now().UnixMilli()
 	for true {
 		receipt, err := client.TransactionReceipt(context.Background(), common.HexToHash(txId))
 		if receipt == nil {
 			if err == nil {
-				logWithTime(fmt.Sprintf("waiting %s tx %s confirming...", txDesc, txId))
+				LogWithTime(fmt.Sprintf("waiting %s tx %s confirming...", txDesc, txId))
 			} else {
-				logWithTime(fmt.Sprintf("get %s tx %s receipt err: %s...", txDesc, txId, err.Error()))
+				LogWithTime(fmt.Sprintf("get %s tx %s receipt err: %s...", txDesc, txId, err.Error()))
 			}
 			time.Sleep(time.Duration(3) * time.Second)
 		} else {
 			if receipt.Status == 1 {
 				break
 			} else {
-				logWithTime(txDesc + " tx exec failed")
+				LogWithTime(txDesc + " tx exec failed")
 				return false
 			}
 		}
 		if time.Now().UnixMilli()-timeStart >= maxQuerySeconds {
-			logWithTime(fmt.Sprintf("get receipt of tx %s time out", txId))
+			LogWithTime(fmt.Sprintf("get receipt of tx %s time out", txId))
 			return false
 		}
 	}
@@ -59,6 +59,6 @@ func waitTxReceipt(client *ethclient.Client, txId string, txDesc string, maxQuer
 	return true
 }
 
-func logWithTime(msg string) {
+func LogWithTime(msg string) {
 	fmt.Printf("%s %s\n", time.Now().UTC().Add(8*time.Hour).Format("2006-01-02 15:04:05"), msg)
 }
