e1997f13f852a8bce942e78513024dc331e293da
warr <123767168@qq.com>
2022-03-08 17:21:11 +0800

    Update airdroputil.go

--- airdroputil/airdroputil.go
+++ airdroputil/airdroputil.go
@@ -129,12 +129,7 @@ func AirdropETHs(paras *AirdropParams, accounts []common.Address, amounts []*big
 	}
 	defer client.Close()
 
-	chainId, err := client.ChainID(context.Background())
-	for err != nil {
-		ethutil.LogWithTime(fmt.Sprintf("get chainId error: %s,sleep 1s...", err.Error()))
-		chainId, err = client.ChainID(context.Background())
-	}
-
+	chainId := ethutil.GetChainID(client)
 	ethutil.LogWithTime(fmt.Sprintf("airdrop chainId: %s", chainId.String()))
 
 	totalAccount := len(accounts)
