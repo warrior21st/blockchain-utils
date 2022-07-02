b0799edb8222d2dac633a70e6f2cc1dfd4323da4
warr <123767168@qq.com>
2022-07-02 23:52:37 +0800

    Update collectutil.go

--- collectutil/collectutil.go
+++ collectutil/collectutil.go
@@ -59,44 +59,42 @@ func CollectTokens(collectParams *CollectTokenParams, privs []string, detailSave
 	}
 }
 
-func CollectETHs(collectParams *CollectTokenParams, privs []string, detailSaveFile string) {
+func CollectETHs(collectParams *CollectTokenParams, privs []string) {
 	client, err := ethclient.Dial(collectParams.Endpoint)
-	if err != nil {
-		panic(err)
-	}
-	tokenSymbol, err := tokenutil.Symbol(client, collectParams.Token)
 	if err != nil {
 		panic(err)
 	}
 
 	chainId := ethutil.GetChainID(client)
-	decimals := 18
+	gasPrice := big.NewInt(int64(collectParams.GasPriceGwei * params.GWei))
 
 	total := len(privs)
 	for i := 0; i < total; i++ {
 		priv := ethutil.HexToECDSAPrivateKey(privs[i])
 		addr := ethutil.PubkeyToAddress(&priv.PublicKey)
-		balance, _ := tokenutil.BalanceOf(client, collectParams.Token, addr)
-		if balance.Cmp(big.NewInt(0)) == 1 {
-			ethutil.LogWithTime(fmt.Sprintf("%s checked %s %s", addr, tokenutil.ConvertAmount(balance, int32(decimals)), tokenSymbol))
-			commonutil.AppendToFile(detailSaveFile, privs[i]+"\n")
+		balance := ethutil.GetBalance(client, addr)
 
-			nonce := ethutil.GetNextNonce(client, addr)
-			ethutil.LogWithTime(fmt.Sprintf("%s current nonce: %d", addr, nonce))
-
-			gasPrice := big.NewInt(int64(collectParams.GasPriceGwei * params.GWei))
-			gasFee := big.NewInt(0).Mul(big.NewInt(21000), gasPrice)
-			if balance.Cmp(gasFee) <= 0 {
-				ethutil.LogWithTime(fmt.Sprintf("%s balance less than or equals to gas fee,continue...", addr))
-			}
-			incomeAmount := balance.Sub(balance, gasFee)
-			incomeTx := ethutil.NewTx(nonce, collectParams.IncomeTo, incomeAmount, uint64(21000), gasPrice, nil)
-			signedIncomeTx := ethutil.SignTx(priv, incomeTx, chainId)
-			txId := ethutil.GetRawTxHash(signedIncomeTx)
-			ethutil.SendRawTx(client, signedIncomeTx)
-
-			ethutil.WaitTxReceiptSuccess(client, txId, fmt.Sprintf("income %d %s from %s", tokenutil.ConvertAmount(balance, int32(decimals)), tokenSymbol, addr), 0)
+		gasFee := big.NewInt(0).Mul(big.NewInt(21000), gasPrice)
+		if balance.Cmp(gasFee) <= 0 {
+			ethutil.LogWithTime(fmt.Sprintf("%s balance less than or equals to gas fee,continue...", addr))
+			continue
 		}
+
+		nonce := ethutil.GetNextNonce(client, addr)
+		ethutil.LogWithTime(fmt.Sprintf("%s current nonce: %d", addr, nonce))
+
+		incomeAmount := balance.Sub(balance, gasFee)
+		incomeTx := ethutil.NewTx(nonce, collectParams.IncomeTo, incomeAmount, uint64(21000), gasPrice, nil)
+		signedIncomeTx := ethutil.SignTx(priv, incomeTx, chainId)
+		txId := ethutil.GetRawTxHash(signedIncomeTx)
+		ethutil.SendRawTx(client, signedIncomeTx)
+
+		ethutil.LogWithTime(fmt.Sprintf("sended tx %s", txId))
+
+		if i > 0 && (i+1)%50 == 0 {
+			ethutil.WaitTxReceiptSuccess(client, txId, fmt.Sprintf("income index %d / %d", i, total), 0)
+		}
+
 		ethutil.LogWithTime(fmt.Sprintf("income progress %d/%d...", i, total-1))
 	}
 }
